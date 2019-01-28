76b75b2f8c8b731fa6249e3d7649e59b74f1bfff
vanri <vanri0415@gmail.com>
2019-01-28 23:33:11 +0900

    host_name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'contacts#new'
   resources :contacts
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
