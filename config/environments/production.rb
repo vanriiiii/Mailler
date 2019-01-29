ab176100717195fbbad316b89c8206e254b3e8a9
vanri <vanri0415@gmail.com>
2019-01-30 00:45:48 +0900

    .env file create, production.rb host_name changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,12 +1,12 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: 'https://sheltered-shelf-82914.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'sheltered-shelf-82914.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
     domain: "heroku.com",
-    address: "smtp.SendGrid.net",
+    address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
     enable_starttls_auto: true
